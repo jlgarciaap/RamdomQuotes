927dedb5a4d4f1f3b23a1249653557e0579fbff0
Juan Luis Garcia <jlgarciaap@gmail.com>
2016-08-28 10:52:42 +0200

    Ramdom Quotes

---
+++ js.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var quote = '', author = '';
+
+function getQuote() {
+    $.ajax({
+        headers: {
+            "X-Mashape-Key": "nZRUYIX9UDmshdpZJWXy6T7u3ubvp18piCZjsnJgyIhpDpyixi",
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat="famous"',
+        success: function(response) {
+            var r = JSON.parse(response);
+
+
+           quote = r.quote;
+          author = r.author;
+
+            $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text='
+                + '"' + quote + '" ' + author);
+
+            $(".text").animate({
+                    opacity: 0
+                }, 500,
+                function() {
+                    $(this).animate({
+                        opacity: 1
+                    }, 500);
+                    $('#text').text("  "+ r.quote + "  ");
+                });
+
+            $(".author").animate({
+                    opacity: 0
+                }, 500,
+                function() {
+                    $(this).animate({
+                        opacity: 1
+                    }, 500);
+                    $('#author').html(r.author);
+                });
+
+            }
+
+    });
+}
+$(document).ready(function() {
+
+    getQuote();
+    //Se llama nada mas cargar la pagina
+
+    $('#new').on('click', getQuote);
+    //Se llama cada vez que pulsamos el boton
+
+});
